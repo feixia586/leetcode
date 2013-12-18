82a026b2ffaef26f08b61514c55954bef9104dd3
Fei Xia <feixia@cs.cmu.edu>
2013-12-18 01:14:25 -0500

    small modification of helloworld_test.cc to test the git

--- src/0_start/helloworld_test.cc
+++ src/0_start/helloworld_test.cc
@@ -2,6 +2,9 @@
 #include "0_start/helloworld.h"
 
 TEST(HelloWorld, SimpleTest) {
-  int res = start(1, 2);
-  ASSERT_EQ(res, 3);
+  int res1 = start(1, 2);
+  ASSERT_EQ(3, res1);
+
+  int res2 = start(0, 0);
+  ASSERT_EQ(0, res2);
 }
