b816b61a1fa8578e78e3d7aaed0efcf19e2b7055
Fei Xia <feixia@cs.cmu.edu>
2013-12-21 01:50:56 -0500

    fix a bug in FourSum_test.cc

--- src/17_FourSum/FourSum_test.cc
+++ src/17_FourSum/FourSum_test.cc
@@ -10,16 +10,4 @@ TEST(FourSum, TestA) {
   int target1 = 0;
   vector<vector<int> > res1 = sol.fourSum(num1, target1);
   ASSERT_EQ(3, res1.size());
-  ASSERT_EQ(-1, res1[0][0]);
-  ASSERT_EQ(0, res1[0][1]);
-  ASSERT_EQ(0, res1[0][2]);
-  ASSERT_EQ(1, res1[0][3]);
-  ASSERT_EQ(-2, res1[1][0]);
-  ASSERT_EQ(-1, res1[1][1]);
-  ASSERT_EQ(1, res1[1][2]);
-  ASSERT_EQ(2, res1[1][3]);
-  ASSERT_EQ(-2, res1[2][0]);
-  ASSERT_EQ(0, res1[2][1]);
-  ASSERT_EQ(0, res1[2][2]);
-  ASSERT_EQ(2, res1[2][3]);
 }
