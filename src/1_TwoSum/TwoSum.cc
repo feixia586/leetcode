a6de9e2e896b7daa69ed2e0b3fd66fa452aa877f
Fei Xia <feixia@cs.cmu.edu>
2013-12-20 21:43:29 -0500

    change a bit

--- src/1_TwoSum/TwoSum.cc
+++ src/1_TwoSum/TwoSum.cc
@@ -1,10 +1,21 @@
 #include <vector>
+#include <algorithm>
+
 using namespace std;
 
 class Solution {
 public:
-  // Time O(n^2); Space O(1)
   vector<int> twoSum(vector<int> &numbers, int target) {
+    return naive_method(numbers, target);
+  }
+  /************************************************************
+   *
+   ************************************************************/
+
+  /************************************************************
+   * Naive Method. Time O(n^2); Space O(1)
+   ************************************************************/
+  vector<int> naive_method(vector<int>& numbers, int target) {
     vector<int> res;
     int idx1, idx2;
     for(size_t i = 0; i < numbers.size(); i++) {
