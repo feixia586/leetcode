0326563b122bea8bd73ed7324aca0eea0b5f3037
Fei Xia <feixia@cs.cmu.edu>
2013-12-26 13:18:05 -0500

    finish Divide Two Integers and its test

--- src/28_DivideTwoIntegers/DivideTwoIntegers.cc
+++ src/28_DivideTwoIntegers/DivideTwoIntegers.cc
@@ -5,13 +5,63 @@ using namespace std;
 class Solution {
 public:
   int divide(int dividend, int divisor) {
-
+    return method_1(dividend, divisor);
+    // return method_2(dividend, divisor);
   }
 
   /************************************************************
    * Method 1
    ************************************************************/
   int method_1(int dividend, int divisor) {
+    long long abs_dend = abs((long long)dividend);
+    long long abs_dsor = abs((long long)divisor);
+
+    long long res = 0;
+    while (abs_dend >= abs_dsor) {
+      long long tmp = abs_dsor;
+      for (int i = 0; abs_dend >= tmp; i++, tmp <<= 1) {
+        abs_dend -= tmp;
+        res += (1 << i);
+      }
+    }
+
+    return ((dividend ^ divisor) >> 31) ? -res : res;
 
   }
+  /************************************************************
+   * Method 2
+   ************************************************************/
+  int method_2(int dividend, int divisor) {
+    long long abs_dend = abs((long long)dividend);
+    long long abs_dsor = abs((long long)divisor);
+
+    long long tmp_dend = abs_dend;
+    long long tmp_dsor = abs_dsor;
+
+    long long tmp = 1;
+    while(tmp_dend > tmp_dsor) {
+      tmp_dsor <<= 1;
+      tmp <<= 1;
+    }
+
+    long long res = 0;
+    if (tmp_dend == tmp_dsor) {
+      res = tmp;
+      return ((dividend ^ divisor) >> 31) ? -res : res;
+    }
+
+    while(tmp_dend >= abs_dsor) {
+      tmp_dsor >>= 1;
+      tmp >>= 1;
+
+      // Don't forget the "if" condition here
+      if(tmp_dend >= tmp_dsor) {
+        res += tmp;
+        tmp_dend -= tmp_dsor;
+      }
+    }
+
+    return ((dividend ^ divisor) >> 31) ? -res : res;
+  }
+
 };
--- src/28_DivideTwoIntegers/DivideTwoIntegers_test.cc
+++ src/28_DivideTwoIntegers/DivideTwoIntegers_test.cc
@@ -8,4 +8,24 @@ TEST(DivideTwoIntegers, TestA) {
   int res1 = sol.divide(-17, 5);
   cout << -17 / 5 << endl;
   ASSERT_EQ(-3, res1);
+
+  int res2 = sol.divide(2, 1);
+  cout << 2 / 1 << endl;
+  ASSERT_EQ(2, res2);
+
+  int res3 = sol.divide(-2147483648, 1);
+  cout << -2147483648 / 1 << endl; // cout << -2147483649 / 1; will output -2147483649
+  ASSERT_EQ(-2147483648, res3);
+
+  int res4 = sol.divide(1, 1);
+  cout << 1 / 1 << endl;
+  ASSERT_EQ(1, res4);
+
+  int res5 = sol.divide(2147483647, 3);
+  cout << 2147483647 / 3 << endl;
+  ASSERT_EQ(715827882, res5);
+
+  int res6 = sol.divide(15, 5);
+  cout << 15 / 5 << endl;
+  ASSERT_EQ(3, res6);
 }
