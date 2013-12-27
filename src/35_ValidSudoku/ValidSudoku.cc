dbe798af64269279ae5817cbaebffa746b75749a
Fei Xia <feixia@cs.cmu.edu>
2013-12-27 17:50:51 -0500

    finish valid sudoku and its test

--- src/35_ValidSudoku/ValidSudoku.cc
+++ src/35_ValidSudoku/ValidSudoku.cc
@@ -1,10 +1,45 @@
 #include <vector>
+#include <map>
 
 using namespace std;
 
 class Solution {
 public:
   bool isValidSudoku(vector<vector<char> > &board) {
+    map<char, bool> row;
+    map<char, bool> col;
+    map<char, bool> subox;
 
+    for (int i = 0; i < 9; i++) {
+      row.clear();
+      col.clear();
+      for (int j = 0; j < 9; j++) {
+        if (board[i][j] != '.') {
+          if (row[board[i][j]]) return false;
+          else row[board[i][j]] = true;
+        }
+
+        if (board[j][i] != '.') {
+          if (col[board[j][i]]) return false;
+          else col[board[j][i]] = true;
+        }
+      }
+    }
+
+    for (int i = 0; i < 9; i+=3) {
+      for (int j = 0; j < 9; j+= 3) {
+        subox.clear();
+        for (int k = 0; k < 9; k++) {
+          int row_idx = i + k / 3;
+          int col_idx = j + k % 3;
+          if (board[row_idx][col_idx] == '.')
+            continue;
+          if (subox[board[row_idx][col_idx]]) return false;
+          else subox[board[row_idx][col_idx]] = true;
+        }
+      }
+    }
+
+    return true;
   }
 };
--- src/35_ValidSudoku/ValidSudoku_test.cc
+++ src/35_ValidSudoku/ValidSudoku_test.cc
@@ -1,6 +1,55 @@
 #include "gtest/gtest.h"
 #include "ValidSudoku.cc"
+#include <iostream>
 
+using namespace std;
+
+void draw_pic(vector<vector<char> > &board) {
+  int num = 9;
+  for(int i = 0; i < num; i++)
+    cout << " _";
+  cout << endl;
+
+  for (int i = 0; i < num; i++) {
+    for (int j = 0; j < num; j++) {
+      if (board[i][j] == '.')
+        cout << "| ";
+      else
+        cout << "|" << board[i][j];
+    }
+    cout << "|" << endl;
+    for (int i = 0; i < num; i++)
+      cout <<" _";
+    cout << endl;
+  }
+
+  }
 TEST(ValidSudoku, TestA) {
   Solution sol;
+
+  char row1[] = "..4...63."; char row2[] = ".........";
+  char row3[] = "5......9."; char row4[] = "...56....";
+  char row5[] = "4.3.....1"; char row6[] = "...7.....";
+  char row7[] = "...5....."; char row8[] = ".........";
+  char row9[] = ".........";
+  vector<char> vec1(row1, row1+9);
+  vector<char> vec2(row2, row2+9);
+  vector<char> vec3(row3, row3+9);
+  vector<char> vec4(row4, row4+9);
+  vector<char> vec5(row5, row5+9);
+  vector<char> vec6(row6, row6+9);
+  vector<char> vec7(row7, row7+9);
+  vector<char> vec8(row8, row8+9);
+  vector<char> vec9(row9, row9+9);
+
+  vector<vector<char> > board1;
+  board1.push_back(vec1); board1.push_back(vec2);
+  board1.push_back(vec3); board1.push_back(vec4);
+  board1.push_back(vec5); board1.push_back(vec6);
+  board1.push_back(vec7); board1.push_back(vec8);
+  board1.push_back(vec9);
+
+  draw_pic(board1);
+  bool res1 = sol.isValidSudoku(board1);
+  ASSERT_FALSE(res1);
 }
