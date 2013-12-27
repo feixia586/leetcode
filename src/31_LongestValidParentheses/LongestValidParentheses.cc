f0e7c3c6d21bceeb40ff2c3f71450568b58e7a59
Fei Xia <feixia@cs.cmu.edu>
2013-12-26 21:41:11 -0500

    fix bug in Longest Valid Parentheses

--- src/31_LongestValidParentheses/LongestValidParentheses.cc
+++ src/31_LongestValidParentheses/LongestValidParentheses.cc
@@ -6,22 +6,22 @@ using namespace std;
 class Solution {
 public:
   int longestValidParentheses(string s) {
-    stack<char> st;
-    int res = 0, tmp = 0;
+    stack<int> st;
+    int res = 0, p = -1;
 
     int len = s.length();
     for (int i = 0; i < len; i++) {
       if (s[i] == '(')
-        st.push('(');
+        st.push(i);
       else {
-        if (!st.empty()) {
-          if (st.top() == '(') {
-            st.pop();
-            tmp += 2;
-            res = tmp > res ? tmp : res;
-          }
-        } else {
-          tmp = 0;
+        if (st.empty())
+          p = i;
+        else {
+          st.pop();
+          if (st.empty())
+            res = res > i - p ? res : i - p;
+          else
+            res = res > i - st.top() ? res : i - st.top();
         }
       }
     }
