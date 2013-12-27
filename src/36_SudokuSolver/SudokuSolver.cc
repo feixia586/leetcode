0c463099426617806241c5ded93ed826a5b58f2f
Fei Xia <feixia@cs.cmu.edu>
2013-12-27 17:54:24 -0500

    add folder about sudoku solver

---
+++ src/36_SudokuSolver/SudokuSolver.cc
@@ -0,0 +1,10 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+  void solveSudoku(vector<vector<char> > &board) {
+
+  }
+};
---
+++ src/36_SudokuSolver/SudokuSolver_test.cc
@@ -0,0 +1,6 @@
+#include "gtest/gtest.h"
+#include "SudokuSolver.cc"
+
+TEST(SudokuSolver, TestA) {
+
+}
