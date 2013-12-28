703dceb3a64f50d89846648f67768eb447fc9faa
Fei Xia <feixia@cs.cmu.edu>
2013-12-28 03:20:32 -0500

    small modification to Sudoku Solver

--- src/36_SudokuSolver/SudokuSolver.cc
+++ src/36_SudokuSolver/SudokuSolver.cc
@@ -7,4 +7,13 @@ public:
   void solveSudoku(vector<vector<char> > &board) {
 
   }
+
+  bool backtrack(vector<vector<char> > &board, int row, int col) {
+    bool get_success = getNextPos(board, row, col);
+  }
+  bool getNextPos(const vector<vector<char> > &board, int &row, int &col) {
+    while (board[row][col] != '.') {
+
+    }
+  }
 };
