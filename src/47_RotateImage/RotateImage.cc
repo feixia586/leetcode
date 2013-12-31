d3b95d00f0eb42b6428596dc4f78049445b20021
Fei Xia <feixia@cs.cmu.edu>
2013-12-30 21:12:19 -0500

    add rotate image and its test

--- src/47_RotateImage/RotateImage.cc
+++ src/47_RotateImage/RotateImage.cc
@@ -5,6 +5,16 @@ using namespace std;
 class Solution {
 public:
   void rotate(vector<vector<int> > &matrix) {
-    vector<vector<int> > cp(matrix);
+    int  n = matrix.size();
+
+    for (int i = 0; i < n/2; i++) {
+      for (int j = i; j < n-i-1; j++) {
+        int tmp = matrix[i][j];
+        matrix[i][j] = matrix[n-j-1][i];
+        matrix[n-j-1][i] = matrix[n-i-1][n-j-1];
+        matrix[n-i-1][n-j-1] = matrix[j][n-i-1];
+        matrix[j][n-i-1] = tmp;
+      }
+    }
   }
 };
