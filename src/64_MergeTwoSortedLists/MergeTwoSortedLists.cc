f9104dc714f73850bdda801cdec7b64bcf809aeb
Fei Xia <feixia@cs.cmu.edu>
2014-01-06 00:31:01 -0500

    add merge two sorted lists framework

---
+++ src/64_MergeTwoSortedLists/MergeTwoSortedLists.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+// Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+  ListNode *mergeTwoLists(ListNode *l1, ListNode *l2) {
+  }
+};
