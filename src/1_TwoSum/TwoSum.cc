8ab2ea789ec7e82c1575ef8f3155d5275d6236f5
Fei <feixia@cs.cmu.edu>
2014-07-09 10:40:23 -0400

    update twosum

--- src/1_TwoSum/TwoSum.cc
+++ src/1_TwoSum/TwoSum.cc
@@ -1,16 +1,46 @@
 #include <vector>
 #include <algorithm>
+#include <unordered_map>
 
 using namespace std;
 
 class Solution {
 public:
   vector<int> twoSum(vector<int> &numbers, int target) {
-    return naive_method(numbers, target);
+    return hashMapMethod(numbers, target);
+    //return naive_method(numbers, target);
   }
+
   /************************************************************
-   *
+   * HashMap Method. Time O(n); Space O(n)
    ************************************************************/
+  vector<int> hashMapMethod(vector<int> &numbers, int target) {
+    vector<int> res;
+    int size = numbers.size();
+    if (size == 0) {
+      return res;
+    }
+
+    unordered_map<int, int> myMap;
+    for (int i = 0; i < size; i++) {
+      myMap[numbers[i]] = i;
+    }
+    for (int i = 0; i < size; i++) {
+      int val = target - numbers[i];
+      if (myMap.find(val) != myMap.end()) {
+        int idx = myMap.find(val)->second;
+        if (idx == i) {
+          continue;
+        }
+        res.push_back(i + 1);
+        res.push_back(idx + 1);
+        return res;
+      }
+    }
+
+    return res;
+  }
+
 
   /************************************************************
    * Naive Method. Time O(n^2); Space O(1)
