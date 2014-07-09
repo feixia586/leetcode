2c869e060bb9f9527107fdff126bec4d418fabb4
Fei <feixia@cs.cmu.edu>
2014-07-08 22:50:47 -0400

    update sort colors

--- src/75_SortColors/SortColors.cc
+++ src/75_SortColors/SortColors.cc
@@ -6,6 +6,38 @@ using namespace std;
 class Solution {
 public:
   void sortColors(int A[], int n) {
+    method_1(A, n);
+    //method_2(A, n);
+  }
+
+  /*******************************************************************
+   * method 1 -- O(n), only one passes
+   *******************************************************************/
+  void method_1(int A[], int n) {
+    if (n <= 1) {
+      return;
+    }
+
+    int pl = 0, pr = n - 1;
+    int i = 0;
+    while (i <= pr) {
+      if (A[i] == 0) {
+        swap(A[pl], A[i]);
+        pl++;
+        i++;
+      } else if (A[i] == 1) {
+        i++;
+      } else {
+        swap(A[i], A[pr]);
+        pr--;
+      }
+    }
+  }
+
+  /*******************************************************************
+   * method 2 -- O(n), but two passes
+   *******************************************************************/
+  void method_2(int A[], int n) {
     if (n <= 1) {
       return;
     }
