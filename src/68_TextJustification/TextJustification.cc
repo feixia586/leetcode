a6fa6dd3c0d99d6424ceb5c0c416df40b49208db
Fei <feixia@cs.cmu.edu>
2014-07-17 19:54:05 -0400

    add text justification

--- src/68_TextJustification/TextJustification.cc
+++ src/68_TextJustification/TextJustification.cc
@@ -6,6 +6,65 @@ using namespace std;
 class Solution {
 public:
   vector<string> fullJustify(vector<string> &words, int L) {
+    vector<string> res;
+    if (words.size() == 0) {
+      return res;
+    }
 
+    int size = words.size();
+    int i = 0, j = 0;
+    while (i < size) {
+      int crntLen = 0;
+
+      while (j < size && crntLen + (int)words[j].length() <= L) {
+        crntLen += (words[j].length() + 1);
+        j++;
+      }
+
+      // if only one word
+      if (j - i == 1) {
+        int extraSpace = L - words[i].size();
+        appendSpace(words[i], extraSpace);
+        res.push_back(words[i]);
+        i = j;
+      } else if (j < size) {
+        int chLen = crntLen - (j - i);
+        int meanSpace = (L - chLen) / (j - i - 1);
+        int leftSpace = (L - chLen) % (j - i - 1);
+        string line;
+        for (int k = i; k < j - 1; k++) {
+          line += words[k];
+          appendSpace(line, meanSpace);
+          if (leftSpace > 0) {
+            line.push_back(' ');
+            leftSpace--;
+          }
+        }
+        line += words[j - 1];
+        res.push_back(line);
+        i = j;
+      } else {
+        int chLen = crntLen - (j - i);
+        int meanSpace = 1;
+        int leftSpace = L - chLen - (j - i - 1);
+        string line;
+        for (int k = i; k < j - 1; k++) {
+          line += words[k];
+          appendSpace(line, meanSpace);
+        }
+        line += words[j - 1];
+        appendSpace(line, leftSpace);
+        res.push_back(line);
+        i = j;
+      }
+    }
+
+    return res;
+  }
+
+  void appendSpace(string &str, int count) {
+    for (int i = 0; i < count; i++) {
+      str.push_back(' ');
+    }
   }
 };
--- src/68_TextJustification/TextJustification_test.cc
+++ src/68_TextJustification/TextJustification_test.cc
@@ -4,4 +4,11 @@
 TEST(TextJustification, TestA) {
   Solution sol;
 
+  vector<string> words1;
+  words1.push_back("This");
+  words1.push_back("is");
+  words1.push_back("an");
+  words1.push_back("exam");
+  vector<string> res1 = sol.fullJustify(words1, 5);
+  ASSERT_EQ(3, res1.size());
 }
