fa26d31b204e8f816062c0a7e145656ef1d91653
Fei Xia <feixia@cs.cmu.edu>
2014-07-25 12:49:42 -0400

    update codes

--- src/4_AddTwoNumbers/AddTwoNumbers.cc
+++ src/4_AddTwoNumbers/AddTwoNumbers.cc
@@ -7,57 +7,45 @@ struct ListNode {
 };
 
 class Solution {
-public:
-  ListNode *addTwoNumbers(ListNode *l1, ListNode *l2) {
-    ListNode* res = new ListNode(0);
-    ListNode* rp = res;
-    ListNode* p1 = l1;
-    ListNode* p2 = l2;
-
-    int flag = 0;
-    while(p1 != NULL && p2 != NULL) {
-      rp->val = p1->val + p2->val + flag;
-      flag = 0;
-      if(rp->val >= 10) {
-        rp->val -= 10;
-        flag = 1;
+  public:
+    ListNode *addTwoNumbers(ListNode *l1, ListNode *l2) {
+      if (l1 == NULL && l2 == NULL) {
+        return NULL;
       }
 
-      p1 = p1->next;
-      p2 = p2->next;
-      if(p1 != NULL && p2 != NULL) {
-        rp->next = new ListNode(0);
-        rp = rp->next;
+      ListNode dummy(0);
+      ListNode *p = &dummy;
+
+      int carry = 0;
+      while (l1 && l2) {
+        int val = l1->val + l2->val + carry;
+        p->next = new ListNode(val % 10);
+
+        carry = val / 10;
+        p = p->next;
+        l1 = l1->next;
+        l2 = l2->next;
       }
+
+      while (l1) {
+        int val = l1->val + carry;
+        p->next = new ListNode(val % 10);
+        carry = val / 10;
+        p = p->next;
+        l1 = l1->next;
+      }
+      while (l2) {
+        int val = l2->val + carry;
+        p->next = new ListNode(val % 10);
+        carry = val / 10;
+        p = p->next;
+        l2 = l2->next;
+      }
+
+      if (carry > 0) {
+        p->next = new ListNode(carry);
+      }
+
+      return dummy.next;
     }
-
-    if(p1 == NULL && p2 != NULL) {
-      for ( ; p2 != NULL; p2 = p2->next) {
-        rp->next = new ListNode(0);
-        rp = rp->next;
-        rp->val = p2->val + flag;
-        flag = 0;
-        if(rp->val >= 10) {
-          rp->val -= 10;
-          flag = 1;
-        }
-      }
-    } else if (p2 == NULL && p1 != NULL) {
-      for ( ; p1 != NULL; p1 = p1->next) {
-        rp->next = new ListNode(0);
-        rp = rp->next;
-        rp->val = p1->val + flag;
-        flag = 0;
-        if(rp->val >= 10) {
-          rp->val -= 10;
-          flag = 1;
-        }
-      }
-    }
-
-    if (flag != 0)
-      rp->next = new ListNode(flag);
-
-    return res;
-  }
 };
