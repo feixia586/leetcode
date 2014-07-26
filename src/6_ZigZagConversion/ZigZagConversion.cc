9dbfe5c3b4a83997befc273efdb4397843cb1f4b
Fei Xia <feixia@cs.cmu.edu>
2014-07-25 22:44:27 -0400

    update zigzag conversion

--- src/6_ZigZagConversion/ZigZagConversion.cc
+++ src/6_ZigZagConversion/ZigZagConversion.cc
@@ -3,57 +3,29 @@
 using namespace std;
 
 class Solution {
-public:
-  string convert(string s, int nRows) {
-    if (nRows == 1)
-      return s;
-
-    string res_str = "";
-    int len = s.length();
-    string* str_arr = new string[nRows];
-    for (int i = 0; i < nRows; i++)
-      str_arr[i] = "";
-
-    // For the first row
-    int k = 0;
-    while(1) {
-      int idx = (k++) * (2 * nRows - 2);
-      if (idx >= len)
-        break;
-      str_arr[0] += s[idx];
-    }
-
-    // For the rows in middle
-    for (int i = 1; i < nRows-1; i++) {
-      k = 0;
-      while(1) {
-        int idx = k * (2 * nRows - 2) + i;
-        if(idx >= len)
-          break;
-        str_arr[i] += s[idx];
-
-        idx = ((k++) + 1) * (2 * nRows - 2) - i;
-        if(idx >= len)
-          break;
-        str_arr[i] += s[idx];
+  public:
+    string convert(string s, int nRows) {
+      if (nRows <= 1) {
+        return s;
       }
+
+      string res;
+      int len = s.length();
+      for (int i = 0; i < nRows; i++) {
+        for (int j = 0, idx = i; idx < len; j++, idx = i + (nRows - 1) * 2 * j) {
+          res.push_back(s[idx]); // '|' direction
+
+          if (i == 0 || i == nRows - 1) {
+            continue;
+          }
+
+          int diagIdx = idx + (nRows - i - 1)  * 2;
+          if (diagIdx < len) {
+            res.push_back(s[diagIdx]); // '/' direction
+          }
+        }
+      }
+
+      return res;
     }
-
-    // For the last row
-    k = 0;
-    while(1) {
-      int idx = (k++) * (2 * nRows - 2)  + (nRows - 1);
-      if(idx >= len)
-        break;
-      str_arr[nRows - 1] += s[idx];
-    }
-
-    for (int i = 0; i < nRows; i++) {
-      res_str += str_arr[i];
-    }
-
-    delete []str_arr;
-
-    return res_str;
-  }
 };
