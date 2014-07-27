8b2c390cca7c24b5b1c6a608f492d71f5a405d8a
Fei Xia <feixia@cs.cmu.edu>
2014-07-27 14:48:00 -0400

    update nth node from end of list

--- src/19_RemoveNthNodeFromEndOfList/RemoveNthNodeFromEndOfList.cc
+++ src/19_RemoveNthNodeFromEndOfList/RemoveNthNodeFromEndOfList.cc
@@ -1,32 +1,37 @@
 #include <iostream>
+#include <assert.h>
 
 // Definition for singly-linked list.
 struct ListNode {
-    int val;
-    ListNode *next;
-    ListNode(int x) : val(x), next(NULL) {}
+  int val;
+  ListNode *next;
+  ListNode(int x) : val(x), next(NULL) {}
 };
 
 class Solution {
 public:
   ListNode *removeNthFromEnd(ListNode *head, int n) {
-    if (head == NULL || n == 0)
+    if (head == NULL || n == 0) {
       return head;
-
-    ListNode *p  = head, *q = head, *r = head;
-    for (int i = 0; i < n; i++)
-      q = q->next;
-
-    while(q != NULL) {r = p; p = p->next; q = q->next;}
-
-    if (p == head) {
-      head = p->next;
-      delete p;
-    } else {
-      r->next = p->next;
-      delete p;
     }
 
-    return head;
+    ListNode dummy(0);
+    dummy.next = head;
+    ListNode *prev = &dummy, *slow = head, *quick = head;
+    for (int i = 0; i < n; i++) {
+      assert(quick != NULL);
+      quick = quick->next;
+    }
+
+    while (quick != NULL) {
+      prev = prev->next;
+      slow = slow->next;
+      quick = quick->next;
+    }
+
+    prev->next = slow->next;
+    delete slow;
+
+    return dummy.next;
   }
 };
