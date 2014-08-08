05ae3511de323e14cfd47b9caeda0dde7e399d94
Fei Xia <feixia@cs.cmu.edu>
2014-08-08 18:41:59 -0400

    update implement strstr

--- src/27_ImplementStrStr/ImplementStrStr.cc
+++ src/27_ImplementStrStr/ImplementStrStr.cc
@@ -4,68 +4,26 @@
 class Solution {
 public:
   char *strStr(char *haystack, char *needle) {
-    //return brute_force_1(haystack, needle);
-    return brute_force_2(haystack, needle);
-  }
-
-  /**********************************************************
-   * Brute force method 1. Time O((n-m+1)*m)
-   **********************************************************/
-  char* brute_force_1(char* haystack, char* needle) {
-    if (haystack == NULL || needle == NULL)
+    if (haystack == NULL || needle == NULL) {
       return NULL;
-    if (!(*needle))
-      return haystack;
+    }
 
-    char *p = haystack, *q = needle;
-    char *pBegin = haystack;
+    int lenH = strlen(haystack);
+    int lenN = strlen(needle);
 
-    char* sig = haystack;
-    while(*(++q)) sig++;
-
-    while (*sig) {
-      p = pBegin;
-      q = needle;
-      while (*p && *q && *p == *q) {
-        p++;
-        q++;
+    for (int i = 0; i < lenH - lenN + 1; i++) {
+      int j = 0;
+      for (j = 0; j < lenN; j++) {
+        if (haystack[i + j] != needle[j]) {
+          break;
+        }
       }
-      if (*q == 0)
-        return pBegin;
-      else {
-        pBegin++;
-        sig++;
+
+      if (j == lenN) {
+        return haystack + i;
       }
     }
 
     return NULL;
   }
-
-  /**********************************************************
-   * Brute force method 2. Time O((n-m+1)*m)
-   **********************************************************/
-  char* brute_force_2(char *haystack, char *needle) {
-    if (haystack == NULL || needle == NULL)
-      return NULL;
-    if (!(*needle))
-      return haystack;
-
-    int n = strlen(haystack);
-    int m = strlen(needle);
-    int p = 0, q = 0;
-    while (p  < n-m+1) {
-      q = 0;
-      while (q < m && haystack[p] == needle[q]) {
-        p++;
-        q++;
-      }
-      if (q == m)
-        return haystack+(p-q);
-      else
-        p = p - q + 1;
-    }
-
-    return NULL;
-  }
-
 };
