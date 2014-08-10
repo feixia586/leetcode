4287e1888b29f4a0cbb36725ffe55187e5cb7cfd
Fei Xia <feixia@cs.cmu.edu>
2014-08-10 15:22:17 -0400

    add DP mehtod

--- src/31_LongestValidParentheses/LongestValidParentheses.cc
+++ src/31_LongestValidParentheses/LongestValidParentheses.cc
@@ -1,12 +1,24 @@
 #include <string>
 #include <stack>
+#include <vector>
 
 using namespace std;
 
 class Solution {
 public:
   int longestValidParentheses(string s) {
+    return stack_Method(s);
+    // return DP_Method(s);
+  }
+
+  /******************************************************************
+   * Stack Method
+   ******************************************************************/
+  int stack_Method(string s) {
     int len = s.length();
+    if (len <= 1) {
+      return 0;
+    }
 
     stack<int> stk;
     int res = 0, pos = -1;
@@ -25,6 +37,40 @@ public:
           }
         }
       }
+    }
+
+    return res;
+  }
+
+  /******************************************************************
+   * DP Method
+   ******************************************************************/
+  int DP_Method(string s) {
+    // state: f[i]: length of the longest valid parentheses that ends with ith character
+    // function: f[i] = f[i-1] + 2 + f[j-1] (where j = i - f[i-1] - 1) if i-th of s == ")" and j-th of s == ")";
+    //                = 0, otherwise, including j < 1
+    // init: f[0] = 0
+    // answer: max(f[i])
+
+    int len = s.length();
+    if (len <= 1) {
+      return 0;
+    }
+
+    vector<int> f(len + 1, 0);
+    for (int i = 1; i <= len; i++) {
+      int j = i - f[i - 1] - 1;
+      if (j < 1) { continue; }
+
+      if (s[i-1] == ')' && s[j-1] == '(') {
+        f[i] = f[i -1] + 2 + f[j - 1];
+      }
+
+    }
+
+    int res = 0;
+    for (int i = 1; i <= len; i++) {
+      res = max(res, f[i]);
     }
 
     return res;
