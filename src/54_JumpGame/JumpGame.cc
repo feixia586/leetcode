96a6ffbc76843c3b8e80398784d51ed2db465e78
Fei Xia <feixia@cs.cmu.edu>
2014-09-07 14:04:53 -0400

    upgrade jump game

--- src/54_JumpGame/JumpGame.cc
+++ src/54_JumpGame/JumpGame.cc
@@ -1,21 +1,56 @@
 #include <algorithm>
+#include <vector>
+#include <assert.h>
 
 using namespace std;
 
 class Solution {
 public:
   bool canJump(int A[], int n) {
-    int minstep = 0, ms_maxdis= 0, cur_maxdis = 0;
-    for (int i = 0; i < n; i++) {
-      if (i == ms_maxdis + 1) {
-        ms_maxdis = cur_maxdis;
-        minstep++;
-      }
+    return DP(A, n);
+    // return greedy(A, n);
+  }
 
-      cur_maxdis = max(cur_maxdis, A[i]+i);
+  /***********************************************************************
+   * DP Method
+   ***********************************************************************/
+  bool DP(int A[], int n) {
+    assert(n > 0);
+
+    vector<bool> canjump(n, false);
+    canjump[0] = true;
+
+    for (int i = 1; i < n; i++) {
+      for (int j = 0; j < i; j++) {
+        if (canjump[j] && A[j] >= i - j) {
+          canjump[i] = true;
+          break;
+        }
+      }
     }
 
-    if (ms_maxdis >= n-1) return true;
-    else return false;
+    return canjump[n - 1];
+  }
+
+  /***********************************************************************
+   * Greedy Method
+   ***********************************************************************/
+  bool greedy(int A[], int n) {
+    assert(n > 0);
+
+    int maxdis = 0, cur_maxdis = 0;
+    for (int i = 0; i < n; i++) {
+      if (i == maxdis + 1) {
+        maxdis = cur_maxdis;
+      }
+
+      cur_maxdis = max(cur_maxdis, i + A[i]);
+    }
+
+    if (maxdis >= n - 1) {
+      return true;
+    } else {
+      return false;
+    }
   }
 };
