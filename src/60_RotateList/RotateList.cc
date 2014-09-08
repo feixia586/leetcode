fecf7b76e99a35b1434ee2b203dc40392ec436a9
Fei Xia <feixia@cs.cmu.edu>
2014-09-07 22:17:14 -0400

    upgrade rotate list

--- src/60_RotateList/RotateList.cc
+++ src/60_RotateList/RotateList.cc
@@ -2,41 +2,47 @@
 
 //Definition for singly-linked list.
 struct ListNode {
-    int val;
-    ListNode *next;
-    ListNode(int x) : val(x), next(NULL) {}
+  int val;
+  ListNode *next;
+  ListNode(int x) : val(x), next(NULL) {}
 };
 
 class Solution {
 public:
   ListNode *rotateRight(ListNode *head, int k) {
-    if (head == NULL || head->next == NULL) return head;
+    if (head == NULL || k == 0) {
+      return head;
+    }
 
     // refine k
-    ListNode* pq = head;
-    int count = 0;
-    while (pq != NULL) { count++; pq = pq->next; }
-    k = k % count;
-    if (k == 0) return head;
+    int len = getLength(head);
+    k = k % len;
 
-    // get ready for modification
     ListNode dummy(0);
     dummy.next = head;
     ListNode *p = &dummy, *q = &dummy;
-    int i = 0;
-    while(i < k) { q = q->next; i++; }
-    while(q->next != NULL) {p = p->next; q = q->next;};
 
-    // modify
-    ListNode* pos = &dummy;
+    for (int i = 0; i < k; i++) { p = p->next; }
     while (p->next != NULL) {
-      ListNode *tmp = p->next;
-      p->next = tmp->next;
-      tmp->next = pos->next;
-      pos->next = tmp;
-      pos = pos->next;
+      p = p->next;
+      q = q->next;
     }
 
+    p->next = head;
+    dummy.next = q->next;
+    q->next = NULL;
+
     return dummy.next;
+  }
+
+  int getLength(ListNode *head) {
+    int n = 0;
+    ListNode *p = head;
+    while (p != NULL) {
+      p = p->next;
+      n++;
+    }
+
+    return n;
   }
 };
