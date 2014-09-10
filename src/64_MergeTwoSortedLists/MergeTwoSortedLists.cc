8c6d4463dc2e708f171dc5e3a44d8c160abf1ef1
Fei Xia <feixia@cs.cmu.edu>
2014-09-09 23:44:08 -0400

    upgrade merge two sorted list

--- src/64_MergeTwoSortedLists/MergeTwoSortedLists.cc
+++ src/64_MergeTwoSortedLists/MergeTwoSortedLists.cc
@@ -10,6 +10,46 @@ struct ListNode {
 class Solution {
 public:
   ListNode *mergeTwoLists(ListNode *l1, ListNode *l2) {
+    if (l1 == NULL) {
+      return l2;
+    }
+    if (l2 == NULL) {
+      return l1;
+    }
+
+    ListNode dummy(0);
+    ListNode *p = &dummy;
+
+    while (l1 != NULL && l2 != NULL) {
+      if (l1->val < l2->val) {
+        p->next = l1;
+        l1 = l1->next;
+      } else {
+        p->next = l2;
+        l2 = l2->next;
+      }
+
+      p = p->next;
+    }
+
+    while (l1 != NULL) {
+      p->next = l1;
+      p = p->next;
+      l1 = l1->next;
+    }
+    while (l2 != NULL) {
+      p->next = l2;
+      p = p->next;
+      l2 = l2->next;
+    }
+
+    return dummy.next;
+  }
+
+  /*****************************************************
+   * Not Recommended
+   *****************************************************/
+  ListNode *anotherMethod(ListNode *l1, ListNode *l2) {
     if (l1 == NULL) return l2;
     else if (l2 == NULL) return l1;
 
@@ -31,5 +71,6 @@ public:
       pre_p->next = q;
 
     return head;
+
   }
 };
