9515e9f3c0ed8a0cf16adad42abd0b3772bf546e
Fei Xia <feixia@cs.cmu.edu>
2014-09-10 20:40:37 -0400

    upgrade add binary

--- src/65_AddBinary/AddBinary.cc
+++ src/65_AddBinary/AddBinary.cc
@@ -7,31 +7,39 @@ using namespace std;
 class Solution {
 public:
   string addBinary(string a, string b) {
-    int lenA = a.length(), lenB = b.length();
-    assert(lenA != 0 && lenB != 0);
+    vector<int> res_vec;
+    string res_str = "";
 
-    vector<char> res_vec;
-    string res = "";
+    int aLen = a.length(), bLen = b.length();
+    int ap = aLen - 1, bp = bLen - 1;
+
     int carry = 0;
-    int p = lenA - 1, q = lenB - 1;
-    while(p >= 0 || q >= 0) {
-      int tmp;
-      if (p >= 0 && q >= 0)
-        tmp = (a[p--] - '0') + (b[q--] - '0') + carry;
-      else if (p >= 0)
-        tmp = (a[p--] - '0') + carry;
-      else
-        tmp = (b[q--] - '0') + carry;
-
-      int val = tmp % 2;
-      carry = tmp / 2;
-      res_vec.push_back(char(val + '0'));
+    while (ap >= 0 && bp >= 0) {
+      int sum = (a[ap--] - '0') + (b[bp--] - '0') + carry;
+      int val = sum % 2;
+      carry = sum / 2;
+      res_vec.push_back(val);
     }
 
-    if (carry > 0) res_vec.push_back(char(carry+'0'));
+    while (ap >= 0) {
+      int sum = (a[ap--] - '0') + carry;
+      carry = sum / 2;
+      res_vec.push_back(sum % 2);
+    }
 
-    int size = res_vec.size();
-    for (int i = size-1; i >= 0; i--) res+= res_vec[i];
-    return res;
+    while (bp >= 0) {
+      int sum = (b[bp--] - '0') + carry;
+      carry = sum / 2;
+      res_vec.push_back(sum % 2);
+    }
+    res_vec.push_back(carry);
+
+    while (res_vec.size() > 1 && res_vec.back() == 0) { res_vec.pop_back(); }
+    for (int i = res_vec.size() - 1; i >= 0; i--) {
+      res_str += (res_vec[i] + '0');
+    }
+
+
+    return res_str;
   }
 };
