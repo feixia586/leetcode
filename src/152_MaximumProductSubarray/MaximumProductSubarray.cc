05279422ab34caadd90c7b202d6bc8df3288d495
Fei Xia <feixia@cs.cmu.edu>
2014-09-25 13:34:45 -0400

    add some comments

--- src/152_MaximumProductSubarray/MaximumProductSubarray.cc
+++ src/152_MaximumProductSubarray/MaximumProductSubarray.cc
@@ -5,6 +5,8 @@ using namespace std;
 
 class Solution {
 public:
+
+  // The main thought is dynamic programming here.
   int maxProduct(int A[], int n) {
     assert(n >= 1);
 
@@ -12,6 +14,8 @@ public:
     int maxHere = A[0], minHere = A[0];
 
     for (int i = 1; i < n; i++) {
+      // have to use tmp to store the val, since the change of maxhere will
+      // affect the calculation in next line
       int tmpMax = max(max(maxHere * A[i], minHere * A[i]), A[i]);
       int tmpMin = min(min(maxHere * A[i], minHere * A[i]), A[i]);
 
