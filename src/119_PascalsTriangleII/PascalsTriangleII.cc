e8dfe4973ce3538f01a8812732dca12ca128841e
Fei <feixia@cs.cmu.edu>
2014-09-29 00:03:03 -0400

    Update PascalsTriangleII.cc

--- src/119_PascalsTriangleII/PascalsTriangleII.cc
+++ src/119_PascalsTriangleII/PascalsTriangleII.cc
@@ -5,15 +5,14 @@ using namespace std;
 class Solution {
 public:
   vector<int> getRow(int rowIndex) {
-    vector<int> res;
+    vector<int> res(rowIndex + 1, 1);
     for (int i = 0; i <= rowIndex; i++) {
-      res.resize(i + 1, 1);
       // Note: must from right to left, otherwise will casue multiple addition
       for (int j = i - 1; j > 0; j--) {
         res[j] = res[j - 1] + res[j];
       }
     }
-
+        
     return res;
   }
 };
