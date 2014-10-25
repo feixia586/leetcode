4eaf349fb15a2546bd171006c7b12eeb13b59733
Fei Xia <feixia@cs.cmu.edu>
2014-10-25 01:33:26 -0400

    upgrade codes

--- src/147_InsertionSortList/InsertionSortList.cc
+++ src/147_InsertionSortList/InsertionSortList.cc
@@ -1,31 +1,38 @@
 #include <iostream>
 // Definition for singly-linked list.
 struct ListNode {
-    int val;
-    ListNode *next;
-    ListNode(int x) : val(x), next(NULL) {}
+  int val;
+  ListNode *next;
+  ListNode(int x) : val(x), next(NULL) {}
 };
 
 
 class Solution {
 public:
   ListNode *insertionSortList(ListNode *head) {
-    if (head == NULL || head->next == NULL) return head;
-
-    ListNode *p = head, *q;
-    ListNode *sp, *pre_sp;
-    while(p->next != NULL) {
-      q = p->next;
-      sp = head;
-      while (sp != q) {
-        if (sp->val <= q->val) { pre_sp = sp; sp = sp->next; }
-        else break;
-      }
-      if (sp == q) p = p->next;
-      else if (sp == head) { head = q;  p->next = q->next; q->next = sp; }
-      else {pre_sp->next = q; p->next = q->next; q->next = sp;}
+    if (head == NULL || head->next == NULL) {
+      return head;
     }
 
-    return head;
+    ListNode dummy(0);
+    dummy.next = head;
+
+    ListNode *crnt = head->next, *next;
+    head->next = NULL; // note this!!!
+
+    ListNode *d;
+    while (crnt != NULL) {
+      next = crnt->next;
+      d = &dummy;
+      while (d->next != NULL && crnt->val > d->next->val) {
+        d = d->next;
+      }
+      crnt->next = d->next;
+      d->next = crnt;
+
+      crnt = next;
+    }
+
+    return dummy.next;
   }
 };
