09dab11ad7b4eca12500473e0dd50360dc0286f9
Fei Xia <feixia@cs.cmu.edu>
2014-10-25 12:45:35 -0400

    upgrade codes

--- src/150_EvaluateReversePolishNotation/EvaluateReversePolishNotation.cc
+++ src/150_EvaluateReversePolishNotation/EvaluateReversePolishNotation.cc
@@ -3,41 +3,56 @@
 #include <assert.h>
 #include <stack>
 #include <string>
+#include <climits>
 
 using namespace std;
 
 class Solution {
 public:
   int evalRPN(vector<string> &tokens) {
-    int size = tokens.size();
-    if (size == 0) {
+    if (tokens.size() == 0) {
       return 0;
     }
 
     stack<int> stk;
-    string ops = "+-*/";
-    for (int i = 0; i < size; i++) {
+    int size = tokens.size();
+    for (int i = 0; i < size; ++i) {
       string str = tokens[i];
-      int idx = ops.find(str);
 
-      if (idx != -1) {
+      if (isOp(str)) {
         assert(stk.size() >= 2);
 
-        int a = stk.top();
-        stk.pop();
-        int b = stk.top();
-        stk.pop();
-        if (idx == 0) { stk.push(b + a); }
-        if (idx == 1) { stk.push(b - a); }
-        if (idx == 2) { stk.push(b * a); }
-        if (idx == 3) { stk.push(b / a); }
-
+        int valB = stk.top(); stk.pop();
+        int valA = stk.top(); stk.pop();
+        stk.push(applyOp(valA, valB, str));
       } else {
-        stk.push(atoi(str.c_str()));
+        stk.push(stoi(str));
       }
     }
 
     assert(stk.size() == 1);
     return stk.top();
+  }
+
+  bool isOp(string str) {
+    if (str == "+" || str == "-" || str == "*" || str == "/") {
+      return true;
+    }
+
+    return false;
+  }
+
+  int applyOp(int valA, int valB, string str) {
+    if (str == "+") {
+      return valA + valB;
+    } else if (str == "-") {
+      return valA - valB;
+    } else if (str == "*") {
+      return valA * valB;
+    } else if (str == "/") {
+      return valA / valB;
+    } else {
+      return INT_MIN;
+    }
   }
 };
