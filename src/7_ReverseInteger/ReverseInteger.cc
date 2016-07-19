0b102898069a08211dc3ed6daf873532729a1118
Fei <feixia@cs.cmu.edu>
2016-07-18 23:33:52 -0700

    Update ReverseInteger.cc

--- src/7_ReverseInteger/ReverseInteger.cc
+++ src/7_ReverseInteger/ReverseInteger.cc
@@ -22,4 +22,26 @@ class Solution {
 
       return res;
     }
+    
+    // More reliable version, since "res >= 0" in the above code doesn't guarantee it doesn't have overflow.
+    // Note that -127 % 10 = -7, -127 / 10 = -12.
+    int reverse(int x) {
+      int sign = (x >= 0 ? 1 : -1);
+        
+      int res = 0;
+      while (x != 0) {
+        int digit = x % 10;
+        if (sign > 0 && res > INT_MAX / 10 || (res == INT_MAX / 10 && digit > INT_MAX % 10)) {
+          return 0;
+        }
+        if (sign < 0 && res < INT_MIN / 10 || (res == INT_MIN / 10 && digit < INT_MIN % 10)) {
+          return 0;
+        }
+            
+        res = res * 10 + digit;
+        x = x / 10;
+      }
+        
+      return res;
+    }
 };
